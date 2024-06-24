ef783ef4acdd047fcac870dfb576228f153d6be8
Yash <saxenay117@gmail.com>
2024-06-24 13:33:30 +0530

    commit

---
+++ app/src/Hero.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import bag from './bag.png'
+import avatar from './avatar.png'
+export default function Hero() {
+  return (
+    <div className='Hero-container'>
+      <Navbar />
+    </div>
+  )
+}
+
+const Navbar = ()=>{
+  return (
+    <div className='navbar'>
+        <div className='logo'>Oak & Ivory</div>
+        <div className='tabs'>
+            <div className='home'>Home</div>
+            <div className='collect'>Collections</div>
+            <div className='abt'>About</div>
+        </div>
+        <div className='icons'>
+            <img src={bag} alt='bag' id='cart'></img>
+            <img src={avatar} alt='avatar' id='profile'></img>
+        </div>
+    </div>
+  )
+}
