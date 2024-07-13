89aedbfea6d4aa2a73dd2580d12bd1a08cb0bfa2
Yash <saxenay117@gmail.com>
2024-07-13 19:40:53 +0530

    sv

--- app/express/index.js
+++ app/express/index.js
@@ -18,5 +18,7 @@ mongoose.connect(mongoURI)
 // Routes
 app.use('/api/users', userRoutes);
 
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
